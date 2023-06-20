d2e1505b4ac3ebd73dd525de9532f7b13a031eea
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-06-20 20:05:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -109,7 +109,7 @@ echo "$localip $(hostname)" >> /etc/hosts
 fi
 mkdir -p /etc/xray
 
-echo -e "${tyblue} Welcome To Arz AutoScript......${NC} "
+echo -e "${tyblue} Welcome To kontol......${NC} "
 sleep 2
 echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
