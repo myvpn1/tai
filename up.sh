c58898ee3e63576fc8b9aa231130bb138e0d2973
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-06-21 00:55:08 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,2 +1,9 @@
-gak ada danjok
-menu
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+echo -e " [INFO] Downloading Update File"
+sleep 2
+echo -e " [INFO] Update Successfully"
+sleep 2
+exit
