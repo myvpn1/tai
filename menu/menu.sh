65f6f4a5bbeb69d36ec9ff768201702ed37783e2
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-06-21 12:19:03 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -21,36 +21,6 @@ ICyan='\033[0;96m'        # Cyan
 IWhite='\033[0;97m'       # White
 NC='\e[0m'
 
-# =========================================
-vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
-let vla=$vlx/2
-vmc=$(grep -c -E "^### " "/etc/xray/config.json")
-let vma=$vmc/2
-ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-
-trx=$(grep -c -E "^#! " "/etc/xray/config.json")
-let tra=$trx/2
-ssx=$(grep -c -E "^## " "/etc/xray/config.json")
-let ssa=$ssx/2
-
-###########- END COLOR CODE -##########
-tram=$( free -h | awk 'NR==2 {print $2}' )
-uram=$( free -h | awk 'NR==2 {print $3}' )
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-# Getting CPU Information
-cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
-cpu_usage+=" %"
-# TOTAL RAM
-total_ram=` grep "MemTotal: " /proc/meminfo | awk '{ print $2}'`
-totalram=$(($total_ram/1024))
-
-persenmemori="$(echo "scale=2; $usmem*100/$tomem" | bc)"
-#persencpu=
-persencpu="$(echo "scale=2; $cpu1+$cpu2" | bc)"
-
-
 # // Export Color & Information
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
@@ -75,7 +45,7 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/tai > /root/tmp
+    curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -93,7 +63,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/tai | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -110,7 +80,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/tai | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -129,7 +99,7 @@ if [ "$res" = "Expired" ]; then
 Exp="\e[36mExpired\033[0m"
 rm -f /home/needupdate > /dev/null 2>&1
 else
-Exp=$(curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/tai | grep $MYIP | awk '{print $3}')
+Exp=$(curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip | grep $MYIP | awk '{print $3}')
 fi
 
 # // Clear
@@ -183,49 +153,24 @@ IPVPS=$(curl -s ipinfo.io/ip )
 ISPVPS=$( curl -s ipinfo.io/org )
 UDPX="https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2"
 clear
-echo -e "${BICyan}  ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "${BICyan}  │  ${BICyan}Use Core        :  ${BIGreen}Pertamax Multiport ${NC}" 
-echo -e " ${BICyan} │  ${BICyan}OS VPS          :  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-` $NC
-echo -e " ${BICyan} │  ${BICyan}Current Domain  :  ${BIBlue}$(cat /etc/xray/domain)${NC}"
-echo -e " ${BICyan} │  ${BICyan}SLOWDNS Domain  :  ${BIBlue}$(cat /root/nsdomain)${NC}"
-echo -e " ${BICyan} │  ${BICyan}Jumlah Ram      :  ${GREEN}${totalram}MB"
-echo -e " ${BICyan} │  ${BICyan}Server Resource :  $uram / $tram"
-echo -e " ${BICyan} │  ${BICyan}CPU Usage       :  $cpu_usage"
-echo -e " ${BICyan} │  ${BICyan}Clients Name    :  ${GREEN}$Name${NC}"
-echo -e " ${BICyan} │  ${BICyan}IP-VPS          :  ${BIBlue}$IPVPS${NC}"
-echo -e " ${BICyan} │  ${BICyan}DATE&TIME       :  $( date -d "0 days" +"%d-%m-%Y | %X" ) ${NC}"
-echo -e " ${BICyan} └─────────────────────────────────────────────────────┘${NC}"
+echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
+echo -e "${BICyan} │  ${BICyan}Use Core        :  ${BIGreen}Pertamax Multiport ${NC}" 
+echo -e " ${BICyan}│  ${BICyan}OS VPS          :  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-` $NC
+echo -e " ${BICyan}│  ${BICyan}Current Domain  :  ${BIBlue}$(cat /etc/xray/domain)${NC}"
+echo -e " ${BICyan}│  ${BICyan}SLOWDNS Domain  :  ${BIBlue}$(cat /root/nsdomain)${NC}"
+echo -e " ${BICyan}│  ${BICyan}IP-VPS          :  ${BIBlue}$IPVPS${NC}"
+echo -e " ${BICyan}│  ${BICyan}ISP-VPS         :  ${BIBlue}$ISPVPS${NC}"
+echo -e " ${BICyan}│  ${BICyan}DATE&TIME       :  $( date -d "0 days" +"%d-%m-%Y | %X" ) ${NC}"
+echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BICyan} SSH ${NC}: $ressh"" ${BICyan} NGINX ${NC}: $resngx"" ${BICyan}  XRAY ${NC}: $resv2r"" ${BICyan} TROJAN ${NC}: $resv2r"
 echo -e "     ${BICyan}          DROPBEAR ${NC}: $resdbr" "${BICyan} SSH-WS ${NC}: $ressshws"
-
-echo -e "${GREEN}┌──────────────────────────────────────────────────┐${NC}" 
-echo -e "${GREEN}│  \033[0m ${BOLD}${YELLOW}SSH     VMESS       VLESS      TROJAN $NC" 
-echo -e "${GREEN}│  \033[0m ${Blue} $ssh1        $vma           $vla          $tra $NC" 
-echo -e "${GREEN}└──────────────────────────────────────────────────┘${NC}" 
-
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "     ${BICyan}[${BIGreen}1${BICyan}]${GREEN} SSH ${NC}"  "        ${BICyan}[${BIGreen}6${BICyan}]${GREEN} BACKUP/RESTORE ${NC}"
-echo -e "     ${BICyan}[${BIGreen}2${BICyan}]${GREEN} VMESS ${NC}"  "      ${BICyan}[${BIGreen}7${BICyan}]${GREEN} SETTINGS ${NC}" 
-echo -e "     ${BICyan}[${BIGreen}3${BICyan}]${GREEN} VLESS ${NC}"  "      ${BICyan}[${BIGreen}8${BICyan}]${GREEN} INFO SCRIPT ${NC}" 
-echo -e "     ${BICyan}[${BIGreen}4${BICyan}]${GREEN} TROJAN ${NC}"  "     ${BICyan}[${BIGreen}9${BICyan}]${GREEN} INFO SERVER ${NC}"
-echo -e "     ${BICyan}[${BIGreen}5${BICyan}]${GREEN} SHADOWSOCKS ${NC}" "${BICyan}[${BIGreen}10${BICyan}]${GREEN} INSTAL UDP ${NC}"       
+echo -e "     ${BICyan}[${BIGreen}1${BICyan}]${GREEN} SSH ${NC}"             "     ${BICyan}[${BIGreen}6${BICyan}]${GREEN} BACKUP/RESTORE ${NC}" 
+echo -e "     ${BICyan}[${BIGreen}2${BICyan}]${GREEN} VMESS ${NC}"           "     ${BICyan}[${BIGreen}7${BICyan}]${GREEN} SETTINGS ${NC}"
+echo -e "     ${BICyan}[${BIGreen}3${BICyan}]${GREEN} VLESS ${NC}"           "     ${BICyan}[${BIGreen}8${BICyan}]${GREEN} INFO SCRIPT ${NC}"
+echo -e "     ${BICyan}[${BIGreen}4${BICyan}]${GREEN} TROJAN ${NC}"          "     ${BICyan}[${BIGreen}9${BICyan}]${GREEN} INFO SERVER ${NC}"
+echo -e "     ${BICyan}[${BIGreen}5${BICyan}]${GREEN} SHADOWSOCKS ${NC}"     "     ${BICyan}[${BIGreen}10${BICyan}]${GREEN} INSTAL UDP ${NC}" 
 echo -e "     ${BICyan}[${BIGreen}x${BICyan}]${GREEN} EXIT ${NC}"  
-echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
-echo -e "${BICyan}                  MENU SETING${NC} "
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "     ${BICyan}[${BIWhite}11${BICyan}] ADD Host/Domain${NC} "   "    ${BICyan}[${BIWhite}16${RED}] Auto Reboot${NC} "
-echo -e "     ${BICyan}[${BIWhite}12${BICyan}] Renew CertXRAY${NC} "   "     ${BICyan}[${BIWhite}17${RED}] Reboot${NC} "
-echo -e "     ${BICyan}[${BIWhite}13${BICyan}] Change Banner SSH${NC} "   "  ${BICyan}[${BIWhite}18${RED}] Speedtest${NC} "
-echo -e "     ${BICyan}[${BIWhite}14${BICyan}] Info Bandwidth${NC} "   "     ${BICyan}[${BIWhite}19${RED}] Limit Speed${NC} "
-echo -e "     ${BICyan}[${BIWhite}15${BICyan}] Restart${NC} "   "            ${BICyan}[${BIWhite}20${RED}] Webmin${NC} "
-echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
-
-echo -e "${BICyan}                  MENU TAMBAHAN${NC} "
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "     ${BICyan}[${BIWhite}21${RED}] DOMAIN FREE${NC} "
-echo -e "     ${BICyan}[${BIWhite}22${RED}] UPDATE MENU${NC} "
-echo -e "     ${BICyan}[${BIWhite}23${RED}] GANTI NS DOMAIN${NC} "
-echo -e "     ${BICyan}[${BIWhite}24${RED}] CLEAR SAMPAH${NC} "
 echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
 DATE=$(date +'%d %B %Y')
 datediff() {
@@ -235,7 +180,7 @@ datediff() {
 }
 mai="datediff "$Exp" "$DATE""
 echo -e " ${BICyan}┌─────────────────────────────────────┐${NC}"
-echo -e " ${BICyan}│  Version       : v5 Last Update ${NC}"
+echo -e " ${BICyan}│  Version       : $(cat /opt/.ver) Last Update ${NC}"
 echo -e " ${BICyan}│  User          :\033[1;36m $Name \e[0m"
 if [ $exp \< 1000 ];
 then
@@ -258,20 +203,6 @@ case $opt in
 8) clear ; info ;;
 9) clear ; infoserv ;;
 10) clear ; clear ; wget --load-cookies /tmp/cookies.txt ${UDPX} -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp ;;
-11) clear ; addhost ;;
-12) clear ; certxray ;;
-13) clear ; nano /etc/issue.net ;;
-14) clear ; cek-bandwidth ;;
-15) clear ; restart ;;
-16) clear ; autoreboot ;;
-17) clear ; reboot ;;
-18) clear ; speedtest ;;
-19) clear ; limitspeed ;;
-20) clear ; webmin ;;
-21) clear ; cf ;;
-22) clear ; up ;;
-23) clear ; slow ;;
-24) clear ; clearlog ;;
 99) clear ; update ;;
 0) clear ; menu ;;
 x) exit ;;
